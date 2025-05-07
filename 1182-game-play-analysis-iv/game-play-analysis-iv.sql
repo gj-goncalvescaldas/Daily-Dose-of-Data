216a385878f2b3610b76417dc3a138265e7d0042
Gustavo João Gonçalves Caldas <gj.goncalvescaldas@gmail.com>
2025-05-07 19:57:29 +0200

    Time: 600 ms (56.78%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,3 +1,4 @@
+/*
 SELECT ROUND(
     COUNT(DISTINCT a1.player_id)  / (SELECT COUNT(DISTINCT player_id) FROM Activity)
     ,2) AS fraction
@@ -8,3 +9,16 @@ JOIN Activity a2
         FROM Activity
         WHERE player_id = a1.player_id
     )
+*/
+select
+    round(count(a2.player_id) / count(a1.player_id), 2) as fraction
+from
+(
+    select
+        player_id,
+        min(event_date) as first_event_date
+    from Activity
+    group by player_id
+) as a1
+left outer join Activity as a2
+on a1.player_id = a2.player_id and a1.first_event_date = date_sub(a2.event_date, interval 1 day)
