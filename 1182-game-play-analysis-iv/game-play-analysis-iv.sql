bccf079fb33a58b5574ec45ab144c2f1e0c0e48d
Gustavo João Gonçalves Caldas <gj.goncalvescaldas@gmail.com>
2025-05-07 20:09:42 +0200

    Time: 575 ms (67.07%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,24 +1,13 @@
-/*
-SELECT ROUND(
-    COUNT(DISTINCT a1.player_id)  / (SELECT COUNT(DISTINCT player_id) FROM Activity)
-    ,2) AS fraction
-FROM Activity a1
-JOIN Activity a2 
-    ON a1.player_id = a2.player_id and DATEDIFF(a2.event_date, a1.event_date) = 1 and a1.event_date = (
-        SELECT MIN(event_date)
-        FROM Activity
-        WHERE player_id = a1.player_id
-    )
-*/
-select
-    round(count(a2.player_id) / count(a1.player_id), 2) as fraction
-from
-(
-    select
-        player_id,
-        min(event_date) as first_event_date
-    from Activity
-    group by player_id
-) as a1
-left outer join Activity as a2
-on a1.player_id = a2.player_id and a1.first_event_date = date_sub(a2.event_date, interval 1 day)
+SELECT 
+    ROUND(
+        count(a2.player_id)/count(a1.player_id)
+        ,2) as fraction
+FROM (
+    SELECT 
+    player_id,
+    MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) AS a1
+LEFT JOIN Activity a2
+    ON a1.player_id = a2.player_id AND a1.first_login = date_sub(a2.event_date, interval 1 day)
