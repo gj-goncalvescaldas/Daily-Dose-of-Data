fa82ff614e74644f5df32adf50f72e6a8f4a63a2
Gustavo João Gonçalves Caldas <gj.goncalvescaldas@gmail.com>
2025-05-09 00:35:11 +0200

    Time: 317 ms (65.94%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,3 +1,31 @@
+/* Primera Solucion
+
+WITH countRequest AS (
+    SELECT requester_id as id, COUNT(*) AS num
+    FROM RequestAccepted 
+    GROUP BY requester_id
+),
+    countAccepter AS (
+    SELECT accepter_id as id, COUNT(*) as num
+    FROM RequestAccepted 
+    GROUP BY accepter_id
+
+),
+
+all_counts AS (
+SELECT id, num FROM countRequest
+UNION ALL
+SELECT id, num FROM countAccepter
+)
+
+SELECT id, SUM(num) AS num
+FROM all_counts
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
+
+*/
+
 SELECT t1.id, COUNT(*) as num FROM
 (
     SELECT requester_id as id FROM RequestAccepted
