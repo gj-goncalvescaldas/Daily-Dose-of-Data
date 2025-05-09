998ff378bc488f01d16ba5da5decd08192e9557b
Gustavo João Gonçalves Caldas <gj.goncalvescaldas@gmail.com>
2025-05-10 01:15:16 +0200

    Time: 510 ms (76.36%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,8 +1,6 @@
-WITH CTE AS (
+
     SELECT P.project_id, ROUND(AVG(E.experience_years), 2) AS average_years
     FROM Project P
     LEFT JOIN Employee E ON E.employee_id = P.employee_id
     GROUP BY P.project_id
     ORDER BY P.project_id
-)
-SELECT * FROM CTE
