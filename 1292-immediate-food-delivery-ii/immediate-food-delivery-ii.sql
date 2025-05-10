f5bfe47a3f47b0e041fecc8f542ce0f9f2a7b2f3
Gustavo João Gonçalves Caldas <gj.goncalvescaldas@gmail.com>
2025-05-10 16:58:07 +0200

    Time: 682 ms (51.86%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -22,3 +22,12 @@ JOIN
 SELECT 
     ROUND(SUM(order_status = 'immediate') * 100.0 / COUNT(*), 2) AS immediate_percentage
 FROM CTE;
+
+/*
+
+select ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) as immediate_percentage
+from delivery where (customer_id, order_date) IN
+(select customer_id, min(order_date) as first_order
+from delivery
+group by customer_id)
+*/
