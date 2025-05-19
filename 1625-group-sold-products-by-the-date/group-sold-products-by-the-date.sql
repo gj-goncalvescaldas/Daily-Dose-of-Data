86782525eeafb7fb479857dbcf58741678cc320b
Gustavo Caldas <gj.goncalvescaldas@gmail.com>
2025-05-19 22:50:26 +0200

    Time: 435 ms (84.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,11 @@
+SELECT
+    sell_date,
+    COUNT(distinct product) as num_sold,
+    GROUP_CONCAT(distinct product ORDER BY product SEPARATOR ',') AS products
+
+
+FROM Activities
+
+GROUP BY sell_date
+
+ORDER BY sell_date ASC
