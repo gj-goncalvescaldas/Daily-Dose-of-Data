13be22e0e32563ef5e8402ca7b76e3a0c66af04f
Gustavo Caldas <gj.goncalvescaldas@gmail.com>
2025-05-21 21:27:56 +0200

    Time: 798 ms (11.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,10 @@
+SELECT
+    employee_id,
+   CASE 
+        WHEN COUNT(*) = 1 THEN MAX(department_id)
+        ELSE MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+    END AS department_id
+
+FROM Employee
+
+GROUP BY employee_id
