58d512b91101a3ada03e974fad1f4a4621422945
Gustavo Caldas <gj.goncalvescaldas@gmail.com>
2025-09-08 16:44:52 +0200

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.69%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def is_no_zero(x: int) -> bool:
+            return '0' not in str(x)
+
+        for a in range(1,n):
+            b = n - a 
+
+            if is_no_zero(a) and is_no_zero(b):
+                return [a,b]
