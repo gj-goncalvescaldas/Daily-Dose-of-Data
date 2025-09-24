47f66368cf919a7b7b6d95fd823b284aea83d8c9
Gustavo Caldas <gj.goncalvescaldas@gmail.com>
2025-09-24 09:07:13 +0200

    Time: 317 ms (22.22%) | Memory: 0B (100.00%) - LeetSync

--- 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
@@ -1,33 +1,11 @@
-WITH invalid_logs AS (
-    SELECT
-        ip,
-        CASE
+SELECT
+  TRIM(ip) AS ip,
+  COUNT(*) AS invalid_count
 
-            -- Has less or more than 4 octets
-            WHEN ip NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)$' THEN 1
+FROM logs
 
-            -- Has leading zeros in any octet (like 01.02.03.04)
-            WHEN ip REGEXP '(^|\\.)0[0-9]+' THEN 1
+WHERE TRIM(ip) NOT REGEXP '^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)$'
 
-            -- Contains numbers greater than 255 in any octet
-            WHEN CAST(SUBSTRING_INDEX(ip, '.', 1) AS UNSIGNED) > 255
-              OR CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) AS UNSIGNED) > 255
-              OR CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) AS UNSIGNED) > 255
-              OR CAST(SUBSTRING_INDEX(ip, '.', -1) AS UNSIGNED) > 255
-            THEN 1
+GROUP BY TRIM(ip)
 
-            ELSE 0
-        END AS is_invalid
-    
-    FROM logs
-
-)
-
-SELECT 
-    ip,
-    SUM(is_invalid) as invalid_count
-
-FROM invalid_logs
-WHERE is_invalid = 1
-GROUP BY ip
-ORDER BY invalid_count DESC, ip DESC
+ORDER BY invalid_count DESC, ip DESC;
